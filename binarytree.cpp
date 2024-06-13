788add73b9ac38a62aac79a2bea05690e46a1ac5
panjiprann <panjipranantya20@gmail.com>
2024-06-13 15:29:22 +0700

    membuat function

--- binarytree.cpp
+++ binarytree.cpp
@@ -2,14 +2,14 @@
 #include <string>
 using namespace std;
 
-class node
+class Node
 {
     public:
     string info;
-    node* leftchild;
-    node* rightchild;
+    Node* leftchild;
+    Node* rightchild;
 
-    node(string i, node* l, node* r)
+    Node(string i, Node* l, Node* r)
     {
         info=i;
         leftchild=l;
@@ -20,7 +20,7 @@ class node
 class BinaryTree
 {
     public:
-    node* ROOT;
+    Node* ROOT;
 
     BinaryTree()
     {
@@ -28,4 +28,16 @@ class BinaryTree
     }
 
     void insert(string element) // insert a node in the binary search tree
+    {
+        Node* newNode = new Node(element, nullptr, nullptr); // Allocate memory for the new node
+        newNode->info = element; // Assign value to the data field of the new code
+        newNode->leftchild = nullptr; // Make the left child of the new node point to null
+
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode); // Locate the node which will be the parent of the node to be insert
+
+        
+
+    }
 }
