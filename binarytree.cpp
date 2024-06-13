09dd89b95095868075be999ef99ad11500350550
panjiprann <panjipranantya20@gmail.com>
2024-06-13 15:35:15 +0700

    menambahkan if dan else if

--- binarytree.cpp
+++ binarytree.cpp
@@ -37,7 +37,19 @@ class BinaryTree
         Node* currentNode = nullptr;
         search(element, parent, currentNode); // Locate the node which will be the parent of the node to be insert
 
-        
+        if (parent == nullptr) // if the parent is NULL (Tree is empty)
+        {
+            ROOT = newNode; // mark the new node as ROOT
+            return; // exit
+        }
 
+        if (element < parent->info) // if the value in the data field of the new node is less thean that of the
+        {
+            parent->leftchild = newNode; // Make the left child of the parent point to the new point
+        }
+        else if (element > parent->info) // if the value in the data field of the new node is greater than
+        {
+            parent->rightchild = newNode; // Make the right child of the parent point to the new node
+        }
     }
 }
